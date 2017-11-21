ed753a31b8b05d359ba0e88af833869ee01a1afe
Rob - Laptop <ras12@albion.edu>
2017-11-20 18:05:59 -0600

    Fix compilation issues, upgrade gradle.

--- app/src/main/java/com/mastersofmemory/flashnumbers/MyApplication.java
+++ app/src/main/java/com/mastersofmemory/flashnumbers/MyApplication.java
@@ -1,15 +1,11 @@
 package com.mastersofmemory.flashnumbers;
 
 import android.app.Application;
-import android.content.Context;
 import android.support.v7.app.AppCompatDelegate;
 
-import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
 
 public class MyApplication extends Application {
-
-    private static Context context;
 
     public void onCreate(){
         super.onCreate();
@@ -19,18 +15,8 @@ public class MyApplication extends Application {
         }
         
         LeakCanary.install(this);
-        Stetho.initializeWithDefaults(this);
-        MyApplication.context = getApplicationContext();
 
         /* Adding this line to prevent vector asset errors on pre SDK 21 devices */
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-    }
-
-    public static MyApplication get(Context context) {
-        return (MyApplication) context.getApplicationContext();
-    }
-
-    public static Context getAppContext() {
-        return MyApplication.context;
     }
 }
